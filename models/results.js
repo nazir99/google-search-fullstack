a9821fc7414e4db6a4f323b152fc275370cace64
nazir99 <95626025+nazir99@users.noreply.github.com>
2022-01-30 12:18:20 +0700

    Fix Schema

--- models/results.js
+++ models/results.js
@@ -2,26 +2,27 @@ const mongoose = require('mongoose')
 
 // Create the results moodel
 //
-mongoose.model(
-  'results',
-
-  {
-    title: String,
-    description: {
-      type: String,
-      required: true
-    },
-    url: {
-      type: String,
-      required: String
-    },
-    links: {
-      title: {
-        type: String
-      },
-      url: {
-        type: String
-      }
+module.exports = mongoose.model('results', {
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  links: [
+    {
+      title: String,
+      url: String
     }
-  }
-)
+  ]
+})
+
+//export results schema to make it available
+
+// module.export = resultsSchema
